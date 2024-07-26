ce6f2f43be7590e32d47692833c9aa82b0559022
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-25 22:26:06 -0300

    create-global-header

---
+++ components/GlobalHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="flex justify-between bg-p-green w-screen p-[1rem]">
+        <h1>LOGO</h1>
+        <nav>
+            <ul>
+                <li>OPTION</li>
+            </ul>
+        </nav>
+    </header>
+</template>
--- components/NavBarComponent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>nav</h1>
-</template>
