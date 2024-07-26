571ec6862837934f0abc476ac0d901ffbcc365aa
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-26 09:56:49 -0300

    remove-tailwind-css-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "vuetify-nuxt-module"],
+  modules: ["vuetify-nuxt-module"],
   runtimeConfig: {
     public: {
       apiUrl: "url",
