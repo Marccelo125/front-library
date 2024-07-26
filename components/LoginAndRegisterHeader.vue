0938f01b8ea8dc2b34371a88d0b977c498a5f3cd
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-26 11:09:53 -0300

    add-login-and-register-header-component

---
+++ components/LoginAndRegisterHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="fixed flex justify-between items-center bg-p-green w-screen px-[2rem]">
+        <div>
+            <v-img width="100px" src="https://cdn.vuetifyjs.com/docs/images/one/logos/vuetify-logo-dark.png" />
+        </div>
+        <nav>
+            <ul class="flex">
+                <li class="">
+                    <v-btn rounded="0" variant="text" size="x-large">
+                        OPTION
+                    </v-btn>
+                </li>
+                
+            </ul>
+        </nav>
+    </header>
+</template>
