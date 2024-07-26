8d42f1366f28e671517f3e52b8bf731a35171437
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-07-25 21:37:41 -0300

    form-test

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,17 @@
 <template>
    <div>
       <div class="row">
-         <div class="col-auto" sm="12" md="4" lg="3">
-
+         <div class="col-12 bg-red-500">
+            <form action="">
+               <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" id="email">
+               </div>
+               <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" class="form-control" id="password">
+               </div>
+            </form>
          </div>
       </div>
    </div>
