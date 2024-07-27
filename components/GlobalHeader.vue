bca220b44d18a70982bb50d9f91f005808fb51d1
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-27 11:42:20 -0300

    remove-globalHeader-and-modify-HomeHeader

--- components/GlobalHeader.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <header class="fixed flex justify-between items-center bg-p-green w-screen px-[2rem]">
-    </header>
-</template>
---
+++ components/HomeHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <v-container class="">
+            <v-row>
+                <v-col>
+                    <h1>Library</h1>
+                </v-col>
+                <v-col>
+                    <v-list class="d-flex">
+                        <v-list-item>
+                            Catálogo
+                        </v-list-item>
+                        <v-list-item>
+                            Serviços
+                        </v-list-item>
+                        <v-list-item>
+                            Suporte
+                        </v-list-item>
+                        <v-list-item>
+                            Contato
+                        </v-list-item>
+                    </v-list>
+                </v-col>
+                <v-col>
+                    <div>
+                        <v-btn class="ma-4">
+                            Criar Conta
+                        </v-btn>                
+                        <v-btn class="">
+                            Entrar
+                        </v-btn>                
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </header>
+</template>
