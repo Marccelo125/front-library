7dc28ca0d820de2c7b4d8cfda6b79b59a28a563a
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-26 22:39:28 -0300

    remove-useless-lines

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,26 +1,4 @@
 <template>
     <header class="fixed flex justify-between items-center bg-p-green w-screen px-[2rem]">
-        <div>
-            <v-img width="100px" src="https://cdn.vuetifyjs.com/docs/images/one/logos/vuetify-logo-dark.png" />
-        </div>
-        <nav>
-            <ul class="flex">
-                <li class="">
-                    <v-btn rounded="0" variant="text" size="x-large">
-                        OPTION
-                    </v-btn>
-                </li>
-                <li class="">
-                    <v-btn rounded="0" variant="text" size="x-large">
-                        OPTION
-                    </v-btn>
-                </li>
-                <li class="">
-                    <v-btn rounded="0" variant="text" size="x-large">
-                        LOGIN
-                    </v-btn>
-                </li>
-            </ul>
-        </nav>
     </header>
 </template>
