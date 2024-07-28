720d45c3ab5572e922e11f6fd037e776492f09f8
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-28 11:34:56 -0300

    add-style-in-to-home-header

--- components/HomeHeader.vue
+++ components/HomeHeader.vue
@@ -4,49 +4,58 @@
     .home-header {
         width: 100vh;
         background-color: $bg-white;
-        .container-header {
-            width: 100%;
-            .header-list-item {
-                h4 {
-                    color: $main-green;
-                }
+        .logo {
+            h1 {
+                font-family: "Limelight", sans-serif;
+                color: $main-green;
             }
         }
+        .container-header {
+            width: 100%;
+        }
+        .login-btn {
+            font-weight: bold;
+            background-color: $main-green;
+        }
+        .register-btn {
+            font-weight: bold;
+            color: $main-green; 
+            border: 2px solid $main-green;
+        }
     }
-
 
 
 </style>
 
 <template>
-    <header class="d-flex justify-center home-header w-100">
-        <v-container class="container-header w-100 ma-0">
+    <header class="d-flex justify-center home-header w-100 pa-0">
+        <v-container class="container-header w-100 ma-0 pa-0">
             <v-row>
-                <v-col>
+                <v-col class="d-flex align-center logo">
                     <h1>Library</h1>
                 </v-col>
                 <v-col class="d-flex justify-center">
                     <v-list class="d-flex bg-transparent">
-                        <v-list-item>
+                        <v-list-item class="header-list-item">
                             <h4>Catálogo</h4>
                         </v-list-item>
-                        <v-list-item>
+                        <v-list-item class="header-list-item">
                             <h4>Serviços</h4>
                         </v-list-item>
-                        <v-list-item>
+                        <v-list-item class="header-list-item">
                             <h4>Suporte</h4>
                         </v-list-item>
-                        <v-list-item>
+                        <v-list-item class="header-list-item">
                             <h4>Contato</h4>
                         </v-list-item>
                     </v-list>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <div>
-                        <v-btn class="ma-4">
+                        <v-btn class="ma-4  register-btn align-center rounded-lg" variant="outlined" height="45px">
                             Criar Conta
                         </v-btn>                
-                        <v-btn class="">
+                        <v-btn class="login-btn text-white align-center rounded-lg" variant="flat" height="45px">
                             Entrar
                         </v-btn>                
                     </div>
