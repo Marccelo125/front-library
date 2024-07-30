e961df033c1f91015b7f43f2e1013991225ccc8c
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-07-30 20:04:21 -0300

    create-element-stars

---
+++ components/Stars.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+    export default {
+        data: () => ({
+            windowWidth: window.innerWidth,
+            windowHeight: window.innerHeight,
+        }),
+    }
+</script>
+
+<template>
+    <v-container class="stars-container" :fluid="true">
+        <v-icon
+            size="25px"
+            
+            :style="{
+                position: 'absolute',
+                top: `${randomNumber(100, windowHeight)}px`,
+                left: `${randomNumber(0, windowWidth)}px`,
+                color: '#4ADE80',
+            }"
+            v-for="n in 25"
+            icon="mdi-star-four-points-outline"
+
+            class="stars-element"
+        />
+    </v-container>
+</template>
+
+<style scoped lang="scss">
+    .stars-container {
+        width: 100%;
+        height: 100dvh;
+        position: absolute;
+        z-index: 1;
+        .stars-element {
+            @for $i from 1 through 25 {
+                &:nth-child(#{$i}) {
+                    animation: starsScaleAnimation random(10) * 1s  ease-in-out infinite;
+                }
+            }
+            @keyframes starsScaleAnimation {
+                0% {
+                    transform: scale(0.8);
+                }
+                50% {
+                    transform: scale(1);
+                }
+                100% {
+                    transform: scale(0.8);
+                }
+            }
+        }
+    }
+
+</style>
