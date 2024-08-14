73dc7e4ce7f426795941bc8fc4759058f891903e
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-08-13 21:06:03 -0300

    create-login-class-and-login-types

--- utils/class/login/login-api.ts
+++ utils/class/login/login-api.ts
@@ -1,7 +1,21 @@
-export default class LoginApi {
-    protected href = useRuntimeConfig().apiUrl;
+import axios from "axios";
+import type { LoginApiType, LoginDataType, LoginErrorResponseType, LoginSuccessResponseType } from "../types/login";
 
-    post() {
-        
+export default class LoginApi implements LoginApiType {
+    protected token = useCookie("XSRF-TOKEN");
+
+    protected client = axios.create({
+        baseURL: useRuntimeConfig().public.apiUrl + '/api',
+        headers: {
+            "X-XSRF-TOKEN": this.token.value
+        },
+        withCredentials: true,
+    })
+
+
+    async login(loginData: LoginDataType): Promise<LoginSuccessResponseType> {
+        const response = await this.client.post('login', loginData);
+
+        return response.data
     }
 }
