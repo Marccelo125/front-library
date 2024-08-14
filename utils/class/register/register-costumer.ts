6a2a6ee5d06af66e2c4ac12ce8a2afe30d2ffa62
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-08-13 21:06:58 -0300

    refactoring-code

--- utils/class/register/register-costumer.ts
+++ utils/class/register/register-costumer.ts
@@ -4,16 +4,14 @@ import RegisterApi from "./register-api";
 export default class RegisterCostumer implements RegisterCostumerType {
     private registerApi = new RegisterApi();
 
-    async register({ name, email, password, }: RegisterReferenciesType): Promise<RegisterResponseType | void> {
+    async register({ name, email, password, }: RegisterReferenciesType): Promise<RegisterResponseType> {
         const userData: RegisterType  = {
             name: name.value.value,
             email: email.value.value,
             password: password.value.value,
         }
-
         const response = await this.registerApi.register(userData);
 
         return response
     }
-
 }
